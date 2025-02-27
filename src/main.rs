5a89f8128f8a7684a86a7e5f5a938657e3ab6784
Davis <davis-wolfermann2005@gmx.de>
2025-02-27 16:29:18 +0100

    New model

--- src/main.rs
+++ src/main.rs
@@ -6,43 +6,114 @@ mod assert;
 mod loss;
 mod metric;
 mod utils;
+mod optimizer;
 
 use crate::activation::ActivationFn;
 use crate::layer::Layer;
 use crate::loss::Loss;
 use crate::metric::Metric;
-use std::ops::Rem;
+use crate::optimizer::Optimizer;
+use crate::utils::argmax;
+use ndarray::{array, Array1, Array2};
+use ndarray_npy::write_npy;
+use plotters::prelude::*;
+use rand::seq::SliceRandom;
+
+fn shuffle_rows(x: &mut Array2<f64>, y: &mut Array1<usize>) {
+    assert_eq!(x.nrows(), y.len(), "The number of rows in the input array must match the number of labels.");
+
+    let nrows = x.nrows();
+    let mut indices: Vec<usize> = (0..nrows).collect();
+    let mut rng = rand::rng();
+    indices.shuffle(&mut rng);
+
+    // Create a new array with the same shape.
+    let mut shuffled_x = Array2::<f64>::default(x.raw_dim());
+    let mut shuffled_y = Array1::<usize>::default(y.raw_dim());
+
+    // For each new row, assign from the shuffled index.
+    for (i, &row_idx) in indices.iter().enumerate() {
+        // Copy the row from the original array.
+        let row = x.row(row_idx);
+        shuffled_x.row_mut(i).assign(&row);
+        shuffled_y[i] = y[row_idx];
+    }
+
+    // Overwrite the original array with the shuffled array.
+    *x = shuffled_x;
+    *y = shuffled_y;
+}
 
 fn main() {
-    let (x, y) = data::create_spiral_dataset(100, 3);
+    println!("Creating spiral dataset...");
+    let (mut x, mut y) = data::create_spiral_dataset(100, 3);
+
+    println!("Shuffling dataset...");
+    shuffle_rows(&mut x, &mut y);
+
+    println!("Saving to npy");
+    write_npy("data_x.npy", &x).unwrap();
+    write_npy("data_y.npy", &y.mapv(|x| x as u32)).unwrap();
+
+    println!("Saving dataset to spiral.png...");
     data::display_spiral_dataset(&x, &y, "spiral.png").unwrap();
 
-    let mut layer1 = layer::Dense::new::<initializer::Xavier>(2, 3);
+    let mut layer1 = layer::Dense::new::<initializer::He>(2, 64);
     let mut activation1 = activation::ReLU::default();
 
-    let mut layer2 = layer::Dense::new::<initializer::Xavier>(3, 3);
+    let mut layer2 = layer::Dense::new::<initializer::He>(64, 3);
     let mut activation2 = activation::Softmax::default();
 
     let loss = loss::CategoricalCrossEntropy::default();
 
-    // Forward
-    let layer1_output = layer1.forward(x.view());
-    let activation1_output = activation1.forward(layer1_output.view());
+    let mut optimizer = optimizer::SGD::new(1.);
 
-    let layer2_output = layer2.forward(activation1_output.view());
-    let activation2_output = activation2.forward(layer2_output.view());
+    #[cfg(debug_assertions)]
+    {
+        write_npy("weights1.npy", layer1.get_weights()).unwrap();
+        write_npy("biases1.npy", layer1.get_biases()).unwrap();
 
-    let loss_value = loss.calculate(&activation2_output, &y);
-    let acc_metric = metric::MultiClassAccuracy::default().evaluate(&activation2_output, &y);
+        write_npy("weights2.npy", layer2.get_weights()).unwrap();
+        write_npy("biases2.npy", layer2.get_biases()).unwrap();
+    }
 
-    // Backward
-    let loss_back = loss.backwards(&activation2_output, &y);
+    let epochs = 10_000;
+    for epoch in 1..=epochs {
+        // Forward
+        let layer1_output = layer1.forward(&x);
+        let activation1_output = activation1.forward(&layer1_output);
 
-    let activation2_back = activation2.backward(&loss_back);
-    let layer2_back = layer2.backward(&activation2_back, 0.01);
+        let layer2_output = layer2.forward(&activation1_output);
+        let activation2_output = activation2.forward(&layer2_output);
 
-    let activation1_back = activation1.backward(&layer2_back);
-    let _layer1_back = layer1.backward(&activation1_back, 0.01);
+        let loss_value = loss.calculate(&activation2_output, &y);
+        let acc_metric = metric::MultiClassAccuracy::default().evaluate(&activation2_output, &y);
 
-    println!("Loss: {:.4}, Accuracy: {:.4}", loss_value, acc_metric);
+        if epoch % 100 == 0 || epoch == 1 {
+            println!("Epoch: {}/{}: Loss: {:?}, Accuracy: {:?}", epoch, epochs, loss_value, acc_metric);
+        }
+
+        // Backward
+        let loss_back = loss.backwards(&activation2_output, &y);
+
+        let activation2_back = activation2.backward(&loss_back);
+        let layer2_back = layer2.backward(&activation2_back);
+
+        let activation1_back = activation1.backward(&layer2_back);
+        let _layer1_back = layer1.backward(&activation1_back);
+
+        optimizer.update(&mut layer1);
+        optimizer.update(&mut layer2);
+    }
+
+    data::visualize_pred(&x, &y, |x, y| {
+        let input = array![[x, y]];
+        let layer1_output = layer1.forward(&input);
+        let activation1_output = activation1.forward(&layer1_output);
+        let layer2_output = layer2.forward(&activation1_output);
+        let activation2_output = activation2.forward(&layer2_output);
+
+        let pred = argmax(&activation2_output);
+        pred[0]
+    }).unwrap();
 }
