0e91c14b0c632c3f1bb0ad66fc4fe7394ac6cfa0
Davis <davis-wolfermann2005@gmx.de>
2025-03-13 19:30:19 +0100

    [Activation] Fix clippy

--- src/activations/linear.rs
+++ src/activations/linear.rs
@@ -21,19 +21,15 @@ mod tests {
 
     #[test]
     fn test_linear_forward() {
-        let mut linear = Linear::default();
-
         let input = array![[1., 2., 3.], [4., 5., 6.]];
-        let output = linear.forward(&input);
+        let output = Linear.forward(&input);
         assert_eq!(output, input);
     }
 
     #[test]
     fn test_linear_backward() {
-        let linear = Linear::default();
-
         let d_values = array![[1., 2., 3.], [4., 5., 6.]];
-        let d = linear.backward(&d_values);
+        let d = Linear.backward(&d_values);
         assert_eq!(d, d_values);
     }
 }
